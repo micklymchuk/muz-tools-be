ec82ef9a341d0fed4dff40be5cfc58d10c7c4a3e
kyka <klymchukkuhcmylk@gmail.com>
2026-03-13 11:05:26 +0200

    Initial commit

---
+++ src/main/java/com/muztools/MuzToolsBeApplication.java
@@ -0,0 +1,13 @@
+package com.muztools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzToolsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzToolsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muztools/MuzToolsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muztools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzToolsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
